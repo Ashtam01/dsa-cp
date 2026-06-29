5896826bf2999d7aaa8692c0db7c232e0d67e762
Ashtam01 <ashtampatitiwari@gmail.com>
2026-06-29 21:27:43 +0530

    KMP algorithm

--- library/strings/kmp.cpp
+++ library/strings/kmp.cpp
@@ -4,48 +4,56 @@
  * Complexity: O(n + m) where n = text length, m = pattern length
  *
  * Usage:
- *   auto lps = build_lps(pattern);
- *   auto matches = kmp_search(text, pattern, lps);
- *   // matches = list of starting indices where pattern occurs
+ * string text = "ABABDABACDABABCABAB";
+ * string pattern = "ABABCABAB";
+ * vector<int> matches = kmp_search(text, pattern);
+ * // matches = list of starting indices where pattern occurs
+ *
+ * Note: The internal pi() function computes the Longest Prefix Suffix (LPS) array.
  */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> build_lps(string& pattern) {
-    int m = pattern.size();
-    vector<int> lps(m, 0);
-    int len = 0, i = 1;
-
-    while (i < m) {
-        if (pattern[i] == pattern[len]) {
-            lps[i++] = ++len;
-        } else if (len) {
-            len = lps[len - 1];
-        } else {
-            lps[i++] = 0;
-        }
-    }
-    return lps;
+vector<int> pi(const string &s) {
+	int n = (int)s.size();
+	vector<int> pi_s(n);
+	for (int i = 1, j = 0; i < n; i++) {
+		while (j > 0 && s[j] != s[i]) { j = pi_s[j - 1]; }
+		if (s[i] == s[j]) { j++; }
+		pi_s[i] = j;
+	}
+	return pi_s;
 }
 
-vector<int> kmp_search(string& text, string& pattern, vector<int>& lps) {
-    int n = text.size(), m = pattern.size();
+vector<int> kmp_search(const string &text, const string &pattern) {
     vector<int> matches;
-    int i = 0, j = 0;
+    if (pattern.empty()) return matches;
 
-    while (i < n) {
+
+    vector<int> pi_s = pi(pattern);
+    
+    int n = (int)text.size();
+    int m = (int)pattern.size();
+
+    for (int i = 0, j = 0; i < n; i++) {
+        
+        
+        while (j > 0 && text[i] != pattern[j]) {
+            j = pi_s[j - 1];
+        }
+        
         if (text[i] == pattern[j]) {
-            i++; j++;
-            if (j == m) {
-                matches.push_back(i - j);
-                j = lps[j - 1];
-            }
-        } else if (j) {
-            j = lps[j - 1];
-        } else {
-            i++;
+            j++;
+        }
+        
+      
+        if (j == m) {
+
+            matches.push_back(i - m + 1);
+            j = pi_s[j - 1]; 
         }
     }
+    
     return matches;
 }
